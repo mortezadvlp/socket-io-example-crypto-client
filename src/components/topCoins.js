0ab9d36bff40c6090121c0146f17a61ff4012027
mortezadvlp <95587714+mortezadvlp@users.noreply.github.com>
2022-02-10 23:02:23 +0330

    Remove extra codes

--- src/components/topCoins.js
+++ src/components/topCoins.js
@@ -1,22 +1,6 @@
-import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import SimpleCoinCard from "./simpleCoinCard";
 
-
-const TopConisContent = () => {
-  const coins = useSelector((state) => state.coins)
-
-  return (
-    <div className='flex flex-row flex-wrap gap-x-4 gap-y-4 justify-center content-center h-screen bg-lime-300' >
-      {
-        coins.map((coin) => 
-          <SimpleCoinCard key={coin.title} title={coin.title} />
-        )
-      }
-      
-    </div>
-  );
-}
 
 export default function TopCoins() {
   const coins = useSelector((state) => state.coins)
